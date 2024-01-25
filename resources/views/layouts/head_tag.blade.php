79d0182fe8d9cb884c52087469637fc42a60f563
Bùi Hữu Lộc <feature451@gmail.com>
2024-01-25 12:59:42 +0700

    fix: force https

--- resources/views/layouts/head_tag.blade.php
+++ resources/views/layouts/head_tag.blade.php
@@ -6,6 +6,7 @@
     <link rel="apple-touch-icon" href="{{ asset('app-assets/images/ico/apple-icon-120.png') }}">
     <link rel="shortcut icon" type="image/x-icon" href="{{ asset('app-assets/images/ico/favicon.ico') }}">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,400;1,500;1,600" rel="stylesheet">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 
     @yield('vendor_css')
     <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/vendors/css/animate/animate.min.css') }}">
